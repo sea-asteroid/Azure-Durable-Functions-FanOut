2b252858bb4099acf7c3425371ace6b0455cff40
sea-asteroid <gongqingyuan@fudan.edu.cn>
2019-11-20 14:28:35 +0100

    Fan-out/in demo

---
+++ DurableFunctionsOrchestratorJS/index.js
@@ -0,0 +1,48 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an HTTP starter function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable activity function (default name is "Hello")
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your 
+ *    function app in Kudu
+ */
+
+const df = require('durable-functions')
+
+module.exports = df.orchestrator(function* (context) {
+  // retrieves the organization name from the Orchestrator_HttpStart function
+  //var organizationName = context.df.getInput();
+  let instanceID = context.df.instanceId;
+  var num_array = new Array(1,2,3); 
+  let organizationName = num_array.length;
+  console.log('Orch start ' + instanceID + ' ' + new Date().getTime() + ' ArrayLength', organizationName);
+  // retrieves the list of repositories for an organization by invoking a separate Activity Function.
+  //var repositories = yield context.df.callActivity('GetAllRepositoriesForOrganization', organizationName);
+  // Creates an array of task to store the result of each functions
+  
+  var output = [];
+  var outputs = [];
+  //for (var i = 0; i < repositories.length; i++) {
+  for (var i = 0; i < organizationName; i++) {
+    // Starting an activity WITHOUT `yield`
+    // This will starts Activity Functions in parallel instead of sequentially.
+    const tmp = context.df.callActivity('Hello', `${num_array[i]} ${instanceID}`);
+    console.log('Orch mid__ ' + instanceID + ' ' + new Date().getTime() + ' after callActivity ', tmp);
+    output.push(tmp);
+    //console.log(instanceID + ' tmp:', tmp);
+    //output.push(context.df.callActivity('Hello', num_array[i]));
+  }
+  // Wait for all Activity Functions to complete execution
+  console.log('Orch yield ' + instanceID + ' ' + new Date().getTime());
+  const results = yield context.df.Task.all(output)
+  //const results = context.df.Task.all(output);
+  outputs.push(results);
+  //console.log('finalOutput:', results);
+  console.log('Orch end__ ' + instanceID + ' ' + new Date().getTime() + ' finished');
+  return outputs;
+  // Send the list to an Activity Function to save them to Blob Storage.
+  //yield context.df.callActivity('SaveRepositories', results)
+  //return context.instanceId
+})
---
+++ Hello/index.js
@@ -0,0 +1,35 @@
+﻿/*
+ * This function is not intended to be invoked directly. Instead it will be
+ * triggered by an orchestrator function.
+ * 
+ * Before running this sample, please:
+ * - create a Durable orchestration function
+ * - create a Durable HTTP starter function
+ * - run 'npm install durable-functions' from the wwwroot folder of your
+ *   function app in Kudu
+ */
+
+module.exports = async function (context) {
+//module.exports = function (context) {
+    //return `Hello ${context.bindings.name}!`;
+    var res = context.bindings.name;
+    var obj = res.split(" ");
+    obj_id = obj[1];
+    obj_count = obj[0];
+    console.log('Act_ start ' + obj_id + ' ' + new Date().getTime());
+    var num = 2000000000;
+    function fibonacci(num){
+        var a = 1, b = 0, temp;
+        while (num >= 0){
+          temp = a;
+          a = a + b;
+          b = temp; 
+          num--;
+        } 
+        return b;
+    }
+    let va = fibonacci(num);
+    console.log('Act_ end__ ' + obj_id + ' ' + new Date().getTime());
+    //return context.bindings.name + 1;
+    return obj_count + 1;
+};
